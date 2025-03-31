c363afb6aacd5f71f772d16628b0f2342c5c3efb
davimessias07 <manualgamer00@gmail.com>
2025-03-30 22:34:32 -0700

    up

--- Scenes/Menu/menu.gd
+++ Scenes/Menu/menu.gd
@@ -2,26 +2,37 @@ extends CanvasLayer
 
 var player_status: Dictionary = {}
 var player_id = ""
-var db_ref = Firebase.Database.get_database_reference("Games")
+var db_games = Firebase.Database.get_database_reference("Games")
 @onready var r_6: Button = $BaseModal/BG/ScreenGridVertical/ScreenGridHorizontal/Body/ScreenGridHorizontal/R6
-
 
 func _ready() -> void:
 	player_id = Firebase.Auth.auth.localid
-	db_ref.new_data_update.connect(new_data_updated)
-	db_ref.patch_data_update.connect(patch_data_updated)
+	
+	db_games.new_data_update.connect(new_data_updated)
+	db_games.patch_data_update.connect(patch_data_updated)
 
-func new_data_updated(data_base):
+func new_data_updated(data):
 	print("new_data_updated")
-	print(data_base)
-	r_6.visible = data_base.data.is_visible
+	print(data)
+	check_state_button(data)
 
 func patch_data_updated(data):
 	print("patch_data_updated")
+	print(data)
 	check_state_button(data)
 
-func _on_r_6_pressed() -> void:
-	db_ref.update("R6", {"is_visible":false})
 
-func check_state_button(data_local):
-	r_6.visible = data_local.data
+func _on_r_6_pressed() -> void:
+	db_games.update("R6",{"is_visible":!r_6.visible})
+
+func check_state_button(state):
+	match state.key:
+		"R6":
+			var visible_state = state.data.is_visible
+			r_6.visible = visible_state
+			printerr(visible_state)
+		"R6/is_visible":
+			var visible_state = state.data
+			r_6.visible = visible_state
+			printerr(visible_state)
+	
