7bd77dbbbab6c9507aafea8d1493f82a198b8e1e
Davi <davio@mobtex.com.br>
2025-04-10 13:24:09 -0300

    up

--- Global/AppManager/load_scene_controller.gd
+++ Global/AppManager/load_scene_controller.gd
@@ -12,13 +12,16 @@ func trade_scene(path_scene:String,args:Dictionary = {}):
 	
 	var new_scene = null
 	var parent_scene = AppManager.scenes
-	var action_complete_trade:Callable
+	var action_complete_trade:Dictionary = {}
 	
 	if args.has("parent_scene"):
 		parent_scene = args.parent_scene
 	
 	if args.has("action_complete"):
-		action_complete_trade = args.action_complete
+		if typeof(args.action_complete) == TYPE_DICTIONARY:
+			action_complete_trade = args.action_complete
+		else :
+			printerr("Tipo incorreto do action_complete")
 	
 	if !parent_scene:
 		printerr("Parent Scene Nulo")
@@ -33,6 +36,27 @@ func trade_scene(path_scene:String,args:Dictionary = {}):
 			new_scene = scene.instantiate()
 			parent_scene.add_child(new_scene)
 			AppManager.current_scene = path_scene
+			
+			if  args.has("action_complete") and action_complete_trade != {}:
+				
+				if action_complete_trade.has("action"):
+					var action = action_complete_trade.action
+					var callable = null
+					
+					if typeof(action) == TYPE_STRING:
+						callable = Callable(new_scene,action)
+					elif typeof(action) == TYPE_CALLABLE:
+						callable = action
+					
+					if callable:
+						if callable.is_valid():
+							if action_complete_trade.has("action_args"):
+								callable.call(action_complete_trade.action_args)
+							else:
+								callable.call()
+			else:
+				printerr("action_complete_trade em Branco")
+				
 		else :
 			printerr("Erro ao carregar Cena")
 	else:
