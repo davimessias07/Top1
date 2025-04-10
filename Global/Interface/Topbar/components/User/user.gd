306ba13e189e65a82491d3d89e322b3f40530ab4
davimessias07 <129984800+davimessias07@users.noreply.github.com>
2025-04-10 16:20:25 -0700

    up

--- Global/Interface/Topbar/components/User/user.gd
+++ Global/Interface/Topbar/components/User/user.gd
@@ -1,8 +1,7 @@
 extends MarginContainer
 
-@onready var username: MarginContainer = $ScreenGridHorizontal/ScreenGridVertical/username
-@onready var balance: MarginContainer = $ScreenGridHorizontal/ScreenGridVertical/balance
-@onready var circle: TextureRect = $ScreenGridHorizontal/Circle
+@onready var circle: TextureRect = $Button/Circle
+@onready var button: Button = $Button
 
 var user_info
 
@@ -10,6 +9,7 @@ var current_prefix = "R$ "
 
 func set_user_attrs():
 	user_info = AppManager.account_controller.data_user
-	var balance_text = current_prefix + str(user_info.balance)
-	username.set_text(user_info.name,20)
-	balance.set_text(balance_text,20)
+	button.connect("pressed",btn_pressed)
+
+func btn_pressed():
+	print("Clicou")
