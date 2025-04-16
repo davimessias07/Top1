aedc9fa55b50cb02dd2c9c591f6a03fb00e686d9
davimessias07 <129984800+davimessias07@users.noreply.github.com>
2025-04-16 14:01:02 -0700

    up

--- Global/Interface/Background/bg.gd
+++ Global/Interface/Background/bg.gd
@@ -1,6 +1,6 @@
 extends TextureRect
 
-var PathsBg = preload("uid://bvbk024pxcxd8").new()
+var Paths_default = preload("uid://bvbk024pxcxd8").new()
 
 @export var disable_get_remote:bool = false
 
@@ -9,14 +9,16 @@ var PathsBg = preload("uid://bvbk024pxcxd8").new()
 func _ready() -> void:
 	if disable_get_remote == false:
 		load_remote_image()
+	else:
+		var img_url = get_default()
+		AppManager.global_functions.getRemoteImage(img_url,self)
 
-func get_bg() -> String:
+func get_default() -> String:
 	match type:
 		"top_bar":
-			return PathsBg.BG_TOP_BAR
-	
-	return PathsBg.BG_DEFAULT
+			return Paths_default.BG_TOP_BAR
+	return Paths_default.BG_DEFAULT
 
 func load_remote_image():
-	var img_url = get_bg()
+	var img_url = get_default()
 	AppManager.global_functions.getRemoteImage(img_url,self)
--- Global/Interface/Background/components/paths_bg.gd
+++ Global/Interface/Background/components/paths_bg.gd
@@ -1,4 +1,3 @@
 extends Node
 
 const BG_DEFAULT:String = "https://pub-4bfee91720814ec5be4a9695b68044df.r2.dev/bg_default.jpg"
-const BG_TOP_BAR:String = "https://pub-4bfee91720814ec5be4a9695b68044df.r2.dev/bg_top_bar.jpg"
