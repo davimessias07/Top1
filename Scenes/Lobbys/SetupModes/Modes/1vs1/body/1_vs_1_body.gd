34345940da6f332d214d8bb9dba7b19649f32326
davimessias07 <129984800+davimessias07@users.noreply.github.com>
2025-05-06 16:51:56 -0700

    up

--- Scenes/Lobbys/SetupModes/Modes/1vs1/body/1_vs_1_body.gd
+++ Scenes/Lobbys/SetupModes/Modes/1vs1/body/1_vs_1_body.gd
@@ -2,7 +2,17 @@ extends Control
 
 @onready var host: Control = $Content/ScreenGridVertical/Users/ScreenGridHorizontal/Host
 @onready var user_2: Control = $Content/ScreenGridVertical/Users/ScreenGridHorizontal/User2
+@onready var btn_state_room: Button = $Content/ScreenGridVertical/btnStateRoom
 
 func set_host_attrs():
 	host.start_setup(AppManager.account_controller.data_user)
-	user_2.username.set_text("Procurando...")
+	user_2.username.set_text("Inimigo")
+
+func set_room_state(new_state):
+	match new_state:
+		"seeking":
+			set_seeking()
+
+func set_seeking():
+	btn_state_room.text = "Esperando Jogador..."
+	btn_state_room.disabled = true
